c44f5a1ad2c80b57c13e01440676da06a5a4becf
Yong He <yonghe@google.com>
2018-12-18 14:24:19 -0800

    Update prepare script

--- prepare.ps1
+++ prepare.ps1
@@ -1,5 +1,6 @@
 if (-NOT ((Test-Path '.\ExternalLibs\FbxSDK\x64\debug\libfbxsdk.lib') -AND (Test-Path '.\ExternalLibs\FbxSDK\x64\release\libfbxsdk.lib') -AND (Test-Path '.\ExternalLibs\FbxSDK\x86\release\libfbxsdk.lib')  -AND (Test-Path '.\ExternalLibs\FbxSDK\x86\debug\libfbxsdk.lib'))) {
 "Required binaries not found, downloading..."
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
 (New-Object Net.WebClient).DownloadFile('https://github.com/csyonghe/SpireMiniEngineExtBinaries/raw/master/binaries.zip', 'binaries.zip')
 Add-Type -assembly "system.io.compression.filesystem"
 if (Test-Path 'ExternalLibs') {
