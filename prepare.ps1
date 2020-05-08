075a57f3102823fa7e14ee26e58fa57d482687d0
Yong He <yonghe@outlook.com>
2020-05-08 05:43:18 -0700

    update prepare.ps1

--- prepare.ps1
+++ prepare.ps1
@@ -2,11 +2,13 @@ Add-Type -assembly "system.io.compression.filesystem"
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
 if (-NOT ((Test-Path '.\ExternalLibs\FbxSDK\x64\debug\libfbxsdk.lib') -AND (Test-Path '.\ExternalLibs\FbxSDK\x64\release\libfbxsdk.lib') -AND (Test-Path '.\ExternalLibs\FbxSDK\x86\release\libfbxsdk.lib')  -AND (Test-Path '.\ExternalLibs\FbxSDK\x86\debug\libfbxsdk.lib'))) {
 "Required binaries not found, downloading..."
-(New-Object Net.WebClient).DownloadFile('https://github.com/csyonghe/SpireMiniEngineExtBinaries/raw/master/binaries.zip', 'binaries.zip')
+(New-Object Net.WebClient).DownloadFile('https://github.com/csyonghe/SpireMiniEngineExtBinaries/raw/master/fbxsdk.zip', 'binaries.zip')
 if (Test-Path 'ExternalLibs') {
 Remove-Item -Recurse -Force 'ExternalLibs'
 }
-[System.IO.Compression.ZipFile]::ExtractToDirectory("binaries.zip", '.\')
+New-Item -ItemType directory -Path "ExternalLibs"
+New-Item -ItemType directory -Path "ExternalLibs\FbxSDK"
+[System.IO.Compression.ZipFile]::ExtractToDirectory("binaries.zip", '.\ExternalLibs\FbxSDK\')
 Remove-Item 'binaries.zip'
 }
 if (Test-path '.\ExternalLibs\Slang\') {
