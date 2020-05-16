117382c4e5e32f3fb264f35e35ed388a9422fc98
Yong He <yonghe@outlook.com>
2020-05-16 07:45:32 -0700

    Improve CI scripts.

--- get_examplegame.sh
+++ get_examplegame.sh
@@ -1,3 +1,3 @@
 wget "https://github.com/spire-engine/engine/releases/download/v0.2/ExampleGame.zip" -O ExampleGame.zip
-unzip ExampleGame.zip
+unzip -q ExampleGame.zip
 rm -f ExampleGame.zip
--- run_test.sh
+++ run_test.sh
@@ -1,8 +1,11 @@
 rm -f rendercommands.txt
 $1 -enginedir ./EngineContent -dir ./ExampleGame -no_renderer -headless -runforframes 3
 if [ $(grep 'Present' rendercommands.txt | wc -l) = 3 ] ; then
-	echo "passed test 'Integration Test'" 
+  rm -f rendercommands.txt
+  echo "passed test 'Integration Test'"
+  exit 0
 else
-  echo "failed test 'Integration Test'" 
+  rm -f rendercommands.txt
+  echo "failed test 'Integration Test'"
+  exit 1
 fi
-rm -f rendercommands.txt
