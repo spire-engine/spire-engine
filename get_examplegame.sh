c999aa79ba78d67c4d7d569b4ad00d41db1aa03f
Yong He <yonghe@outlook.com>
2020-05-16 13:05:50 -0700

    Update asset download script.

--- get_examplegame.sh
+++ get_examplegame.sh
@@ -1,3 +1,3 @@
-wget "https://github.com/spire-engine/engine/releases/download/v0.2/ExampleGame.zip" -O ExampleGame.zip
+wget "https://github.com/spire-engine/spire-engine/releases/download/v0.2/ExampleGame.zip" -O ExampleGame.zip
 unzip -q ExampleGame.zip
 rm -f ExampleGame.zip
